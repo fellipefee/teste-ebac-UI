24fabb21299f21da3ecaf340ac6c2969fd2292c3
Felipe Rubim <fellipe_lipe1@hotmail.com>
2023-07-01 14:23:16 -0300

    usando massa de dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,18 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
-context('Funcionalidade Login', () =>{
+context('Funcionalidade Login', () => {
+
+    beforeEach(() => {
+        cy.visit('minha-conta')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
 
     it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+
 
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
@@ -12,8 +21,24 @@ context('Funcionalidade Login', () =>{
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, Aluno (não é Aluno? Sair)')
     })
 
+    it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.page-title').should('contain', 'Minha conta')
+        })
+    });
+
     it('deve exibir uma mensagem de erro ao inserir usuario ou senha inválidos', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+
         cy.get('#username').type('aluno_ebac123@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -22,7 +47,7 @@ context('Funcionalidade Login', () =>{
     })
 
     it('deve exibir uma mensagem de erro ao inserir senha inválidos', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@')
         cy.get('.woocommerce-form > .button').click()
